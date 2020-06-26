42b5bfb02c077a003386265ccbde5dcf4fdaac4e
Ian Scott <chesnekov@gmail.com>
2020-06-26 09:14:06 -0700

    add logic to successfully pass first test

--- __tests__/galactic-age-calculator.tests.js
+++ __tests__/galactic-age-calculator.tests.js
@@ -1,8 +1,8 @@
-import {GalacticAge} from './..sr/galacti-age-calculator.js'
+import {GalacticAge} from './../src/galactic-age-calculator'
 
 describe ('age', ()=> {
   
-  test('should store and retrieve a users submitted age', () => {
+  test('should store and retrieve a users submitted age in Earth years', () => {
     let sampleUser = new GalacticAge(32);
     expect(sampleUser.earthAge).toBe(32);
   });
